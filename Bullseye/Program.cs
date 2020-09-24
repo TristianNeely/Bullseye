eade52babe4a4328c80c5ffaf1f7c0f96a06a12c
Tristian Neely <Neely_Tristian@student.mahoningctc.com>
2020-09-24 10:12:21 -0400

    Add project files.

---
+++ Bullseye/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bullseye
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
